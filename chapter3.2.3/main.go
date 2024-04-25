5a09b241f6f1510c454038b646477c2feacdd224
Eduardo Luz <luz.eduardo@gmail.com>
2024-04-26 00:10:48 +0100

    addiing concurrency to download files but facing race condition on countLetters

--- chapter3.2.3/main.go
+++ chapter3.2.3/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
@@ -25,11 +26,15 @@ func countLetters(url string, frequency []int) {
 }
 
 func main() {
+	//all goroutines will share the same data structure in memory
+	//and it will add a race condition
 	var frequency = make([]int, 26)
 	for i := 1000; i <= 1020; i++ {
 		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		countLetters(url, frequency)
+		go countLetters(url, frequency)
 	}
+	//forcing the main thread to not finish before goroutines finishes their jobs
+	time.Sleep(5 * time.Second)
 
 	for i, c := range allLetters {
 		fmt.Printf("%c-%d\n", c, frequency[i])

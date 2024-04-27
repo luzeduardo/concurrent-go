5328c9f854228a7ccc7c304bd2ca36ae4a12a1f4
Eduardo Luz <luz.eduardo@gmail.com>
2024-04-27 17:22:31 +0100

    fix race condition of shared memory with mutex

--- chapter3.2.3/main.go
+++ chapter3.2.3/main.go
@@ -5,12 +5,13 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
-func countLetters(url string, frequency []int) {
+func countLetters(url string, frequency []int, mutex *sync.Mutex) {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(resp.Body)
@@ -19,7 +20,9 @@ func countLetters(url string, frequency []int) {
 		c := strings.ToLower(string(b))
 		cIndex := strings.Index(allLetters, c)
 		if cIndex >= 0 {
+			mutex.Lock()
 			frequency[cIndex] += 1
+			mutex.Unlock()
 		}
 	}
 	fmt.Println("Completed: ", url)
@@ -29,16 +32,19 @@ func main() {
 	//all goroutines will share the same data structure in memory
 	//and it will add a race condition
 	var frequency = make([]int, 26)
+	var mutex = sync.Mutex{}
 	for i := 1000; i <= 1020; i++ {
 		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		go countLetters(url, frequency)
+		go countLetters(url, frequency, &mutex)
 	}
 	//forcing the main thread to not finish before goroutines finishes their jobs
 	time.Sleep(5 * time.Second)
 	var allLetterSum int
 	for i, c := range allLetters {
+		mutex.Lock()
 		allLetterSum += frequency[i]
 		fmt.Printf("%c-%d\n", c, frequency[i])
+		mutex.Unlock()
 	}
 	fmt.Printf("Letters in docs: %d\n", allLetterSum)
 }
--- chapter3.7/main.go
+++ chapter3.7/main.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func stingy(money *int) {
+func stingy(money *int, mutex *sync.Mutex) {
 	for i := 0; i < 1000000; i++ {
+		mutex.Lock()
 		*money += 10
+		mutex.Unlock()
 	}
 	fmt.Println("Stingy done")
 }
 
-func spendy(money *int) {
+func spendy(money *int, mutex *sync.Mutex) {
 	for i := 0; i < 1000000; i++ {
+		mutex.Lock()
 		*money -= 10
+		mutex.Unlock()
 	}
 	fmt.Println("Speny done")
 }
 
 func main() {
 	money := 100
-	go stingy(&money)
-	go spendy(&money)
+	mutex := sync.Mutex{}
+	go stingy(&money, &mutex)
+	go spendy(&money, &mutex)
 	time.Sleep(2 * time.Second)
+	mutex.Lock()
 	println("Money in banck account: ", money)
+	mutex.Unlock()
 }

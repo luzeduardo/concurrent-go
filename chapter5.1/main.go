397ffd96f9705c761b739482d32b55bf6a4c89c5
Eduardo Luz <luz.eduardo@gmail.com>
2024-05-08 07:13:56 +0100

    use conditional variables to block and wait until the bank account has enought money

--- chapter5.1/main.go
+++ chapter5.1/main.go
@@ -7,24 +7,38 @@ import (
 	"time"
 )
 
-func stingy(money *int, mutex *sync.Mutex) {
+func stingy(money *int, cond *sync.Cond) {
 	for i := 0; i < 1000000; i++ {
-		mutex.Lock()
+		cond.L.Lock()
 		*money += 10
-		mutex.Unlock()
+		// Signal -> After updating the shared state above,  the condition unlocks the mutex
+		//   - So other gouroutine will receive this Signal and wake up, and reaqcquire the mutex to proceed with its job
+		cond.Signal()
+		cond.L.Unlock()
 	}
 	fmt.Println("Stingy Done")
 }
 
-func spendy(money *int, mutex *sync.Mutex) {
+func spendy(money *int, cond *sync.Cond) {
 	for i := 0; i < 200000; i++ {
-		mutex.Lock()
+		cond.L.Lock()
+
+		for *money < 50 {
+			// Wait -> performs two operations atomically:
+			//  - releases the mutex
+			//  - blocks the current execution, effectivelly putting the goroutine to sleep
+			//  Since the mutex is now available the other goroutine can acquire it to continue with its execution
+			//
+			//  So the wait function releases the mutex and suspends the current goroutine execution in an atomic manner
+			cond.Wait()
+		}
+
 		*money -= 50
 		if *money < 0 {
 			fmt.Println("Money is negative!")
 			os.Exit(1)
 		}
-		mutex.Unlock()
+		cond.L.Unlock()
 	}
 	fmt.Println("Spendy Done")
 }
@@ -32,9 +46,12 @@ func spendy(money *int, mutex *sync.Mutex) {
 func main() {
 	money := 100
 	mutex := sync.Mutex{}
-	go stingy(&money, &mutex)
-	go spendy(&money, &mutex)
+	cond := sync.NewCond(&mutex)
+
+	go stingy(&money, cond)
+	go spendy(&money, cond)
 	time.Sleep(2 * time.Second)
+	mutex.Lock()
 	println("Money in bank account: ", money)
 	mutex.Unlock()
 }

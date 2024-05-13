387c1406805452cd8e8a3ccf9b34c0edb97b407f
Eduardo Luz <luz.eduardo@gmail.com>
2024-05-13 14:33:24 +0100

    implementation of wait groups using semaphores

---
+++ chapter6.3/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Semaphore struct {
+	permits int
+	cond    *sync.Cond
+}
+
+type WaitGrp struct {
+	sema *Semaphore
+}
+
+func NewWaitGrp(size int) *WaitGrp {
+	return &WaitGrp{sema: NewSemaphore(1 - size)}
+}
+
+func (wg *WaitGrp) Wait() {
+	wg.sema.Acquire()
+}
+
+func (wg *WaitGrp) Done() {
+	wg.sema.Release()
+}
+
+func NewSemaphore(n int) *Semaphore {
+	return &Semaphore{
+		permits: n,
+		cond:    sync.NewCond(&sync.Mutex{}),
+	}
+}
+
+func (rw *Semaphore) Acquire() {
+	rw.cond.L.Lock()
+
+	for rw.permits <= 0 {
+		rw.cond.Wait()
+	}
+
+	rw.permits--
+	rw.cond.L.Unlock()
+}
+
+func (rw *Semaphore) Release() {
+	rw.cond.L.Lock()
+
+	rw.permits++
+	rw.cond.Signal()
+
+	rw.cond.L.Unlock()
+}
+
+func main() {
+	semaphore := NewSemaphore(0)
+	for i := 0; i < 20; i++ {
+		go doWork(semaphore, i)
+
+		semaphore.Acquire()
+		fmt.Println("Child goroutine finished: ", i)
+	}
+}
+
+func doWork(semaphore *Semaphore, i int) {
+	fmt.Println("Work started: ", i)
+	fmt.Println("Work Finished ", i)
+
+	semaphore.Release()
+}
